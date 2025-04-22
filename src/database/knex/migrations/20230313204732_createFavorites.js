ebb3c346b0fd71cf1c218b0def826e904c25c221
jhoni <jhoni.psy@hotmail.com>
2025-04-22 13:58:43 +0100

    commit

--- src/database/knex/migrations/20230313204732_createFavorites.js
+++ src/database/knex/migrations/20230313204732_createFavorites.js
@@ -1,4 +1,4 @@
-exports.up = knex => knex.schema.createTable("favorites", table => {
+ knex.schema.createTable("favorites", table => {
   table.increments("id");
 
   table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const usersRouter = require("./users.routes");
 const dishesRouter = require("./dishes.routes");
 const sessionsRouter = require("./sessions.routes");
 const ordersRouter = require("./orders.routes");
-const favoritesRouter = require("./favorites.routes");
 const cartsRouter = require("./carts.routes");
 
 const routes = Router();
@@ -12,7 +11,6 @@ routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
 routes.use("/dishes", dishesRouter);
 routes.use("/orders", ordersRouter);
-routes.use("/favorites", favoritesRouter);
 routes.use("/carts", cartsRouter);
 
 module.exports = routes;
